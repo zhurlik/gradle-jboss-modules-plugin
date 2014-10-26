c2fb22206b411c1ec0e0a1752f6653dc419b5277
zhurlik <zhurlik@pisem.net>
2014-10-26 21:18:05 +0300

    added empty gradle plugin

---
+++ src/main/groovy/com/zhurlik/JBossModulesPlugin.groovy
@@ -0,0 +1,16 @@
+package com.zhurlik
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Gradle plugin to make JBoss Modules.
+ *
+ * @author zhurlik@gmail.com
+ */
+class JBossModulesPlugin implements Plugin<Project> {
+    @Override
+    void apply(final Project project) {
+        println '>> Plugin: JBoss Modules'
+    }
+}

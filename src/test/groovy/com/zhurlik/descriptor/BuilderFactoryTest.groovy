45b1469853a1b5a76b6a48a32942dfe0337fbbd3
zhurlik <zhurlik@pisem.net>
2014-12-12 00:22:50 +0300

    coverage

--- src/test/groovy/com/zhurlik/descriptor/BuilderFactoryTest.groovy
+++ src/test/groovy/com/zhurlik/descriptor/BuilderFactoryTest.groovy
@@ -21,9 +21,12 @@ class BuilderFactoryTest {
 
     @Test
     public void testMain() throws Exception {
-        def none = BuilderFactory.NONE
+        def none = BuilderFactory.getBuilder(null)
         assertNotNull none
         assertNull none.xsd
+        assertNull none.getPath(null)
+        assertNull none.getVersion()
+
         try {
             none.getXmlDescriptor(new JBossModule('test'))
             assert false
@@ -31,10 +34,10 @@ class BuilderFactoryTest {
             assert true
         }
 
-        def ver1_0 = BuilderFactory.<JBossModule> getBuilder(V_1_0)
-        def ver1_1 = BuilderFactory.<JBossModule> getBuilder(V_1_1)
-        def ver1_2 = BuilderFactory.<JBossModule> getBuilder(V_1_2)
-        def ver1_3 = BuilderFactory.<JBossModule> getBuilder(V_1_3)
+        def ver1_0 = BuilderFactory.getBuilder(V_1_0)
+        def ver1_1 = BuilderFactory.getBuilder(V_1_1)
+        def ver1_2 = BuilderFactory.getBuilder(V_1_2)
+        def ver1_3 = BuilderFactory.getBuilder(V_1_3)
 
         assert ver1_0 instanceof Xsd1_0
         assert ver1_1 instanceof Xsd1_1
@@ -48,10 +51,10 @@ class BuilderFactoryTest {
         assertNotEquals ver1_1, ver1_3
         assertNotEquals ver1_2, ver1_3
 
-        assertEquals ver1_0, BuilderFactory.<JBossModule> getBuilder(V_1_0)
-        assertEquals ver1_1, BuilderFactory.<JBossModule> getBuilder(V_1_1)
-        assertEquals ver1_2, BuilderFactory.<JBossModule> getBuilder(V_1_2)
-        assertEquals ver1_3, BuilderFactory.<JBossModule> getBuilder(V_1_3)
+        assertEquals ver1_0, BuilderFactory.getBuilder(V_1_0)
+        assertEquals ver1_1, BuilderFactory.getBuilder(V_1_1)
+        assertEquals ver1_2, BuilderFactory.getBuilder(V_1_2)
+        assertEquals ver1_3, BuilderFactory.getBuilder(V_1_3)
 
         assertEquals 'xsd/module-1_0.xsd', V_1_0.xsd
         assertEquals '1.0', V_1_0.number

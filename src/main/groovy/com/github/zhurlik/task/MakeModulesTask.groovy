4b5625b15a6e226b7e57312915011e567ae35cd3
Mark Vieira <portugee@gmail.com>
2015-09-03 11:23:13 -0700

    Fix error message when no servers have been configured.

--- src/main/groovy/com/github/zhurlik/task/MakeModulesTask.groovy
+++ src/main/groovy/com/github/zhurlik/task/MakeModulesTask.groovy
@@ -2,6 +2,7 @@ package com.github.zhurlik.task
 import com.github.zhurlik.extension.JBossModule
 import groovy.util.logging.Slf4j
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
 /**
@@ -19,7 +20,7 @@ class MakeModulesTask extends DefaultTask {
         logger.info ">> Creating JBoss Modules locally..."
 
         if (project.jbossrepos.isEmpty()) {
-            throw new TaskExecutionException('You need at least one JBoss Server');
+            throw new GradleException('You need at least one JBoss Server');
         }
 
         project.modules.each() { JBossModule m ->

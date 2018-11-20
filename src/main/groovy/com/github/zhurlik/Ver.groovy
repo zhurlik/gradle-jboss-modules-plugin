05b484707935ef6160c3f7630b2a28fdd44737da
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-11-20 14:19:33 +0300

    added more supported versions

--- src/main/groovy/com/github/zhurlik/Ver.groovy
+++ src/main/groovy/com/github/zhurlik/Ver.groovy
@@ -1,6 +1,15 @@
 package com.github.zhurlik
 
-import com.github.zhurlik.descriptor.*
+import com.github.zhurlik.descriptor.Builder
+import com.github.zhurlik.descriptor.Xsd1_0
+import com.github.zhurlik.descriptor.Xsd1_1
+import com.github.zhurlik.descriptor.Xsd1_2
+import com.github.zhurlik.descriptor.Xsd1_3
+import com.github.zhurlik.descriptor.Xsd1_5
+import com.github.zhurlik.descriptor.Xsd1_6
+import com.github.zhurlik.descriptor.Xsd1_7
+import com.github.zhurlik.descriptor.Xsd1_8
+import com.github.zhurlik.descriptor.Xsd1_9
 import com.github.zhurlik.extension.JBossModule
 
 /**
@@ -14,7 +23,10 @@ public enum Ver {
     V_1_2('1.2', 'xsd/module-1_2.xsd', Xsd1_2),
     V_1_3('1.3', 'xsd/module-1_3.xsd', Xsd1_3),
     V_1_5('1.5', 'xsd/module-1_5.xsd', Xsd1_5),
-    V_1_6('1.6', 'xsd/module-1_6.xsd', Xsd1_6);
+    V_1_6('1.6', 'xsd/module-1_6.xsd', Xsd1_6),
+    V_1_7('1.7', 'xsd/module-1_7.xsd', Xsd1_7),
+    V_1_8('1.8', 'xsd/module-1_8.xsd', Xsd1_8),
+    V_1_9('1.9', 'xsd/module-1_9.xsd', Xsd1_9);
 
     private String number
     private String xsd
---
+++ src/test/groovy/com/github/zhurlik/VerTest.groovy
@@ -0,0 +1,58 @@
+package com.github.zhurlik
+
+import com.github.zhurlik.descriptor.Xsd1_0
+import com.github.zhurlik.descriptor.Xsd1_1
+import com.github.zhurlik.descriptor.Xsd1_2
+import com.github.zhurlik.descriptor.Xsd1_3
+import com.github.zhurlik.descriptor.Xsd1_5
+import com.github.zhurlik.descriptor.Xsd1_6
+import com.github.zhurlik.descriptor.Xsd1_7
+import com.github.zhurlik.descriptor.Xsd1_8
+import com.github.zhurlik.descriptor.Xsd1_9
+import org.junit.Test
+
+import static junit.framework.Assert.assertEquals
+import static junit.framework.Assert.assertTrue
+
+class VerTest {
+    @Test
+    void testMain() {
+        assertEquals('[V_1_0, V_1_1, V_1_2, V_1_3, V_1_5, V_1_6, V_1_7, V_1_8, V_1_9]', Ver.values().toArrayString())
+
+        assertTrue(Ver.V_1_0.builder instanceof Xsd1_0)
+        assertEquals('1.0', Ver.V_1_0.number)
+        assertEquals('xsd/module-1_0.xsd', Ver.V_1_0.xsd)
+
+        assertTrue(Ver.V_1_1.builder instanceof Xsd1_1)
+        assertEquals('1.1', Ver.V_1_1.number)
+        assertEquals('xsd/module-1_1.xsd', Ver.V_1_1.xsd)
+
+        assertTrue(Ver.V_1_2.builder instanceof Xsd1_2)
+        assertEquals('1.2', Ver.V_1_2.number)
+        assertEquals('xsd/module-1_2.xsd', Ver.V_1_2.xsd)
+
+        assertTrue(Ver.V_1_3.builder instanceof Xsd1_3)
+        assertEquals('1.3', Ver.V_1_3.number)
+        assertEquals('xsd/module-1_3.xsd', Ver.V_1_3.xsd)
+
+        assertTrue(Ver.V_1_5.builder instanceof Xsd1_5)
+        assertEquals('1.5', Ver.V_1_5.number)
+        assertEquals('xsd/module-1_5.xsd', Ver.V_1_5.xsd)
+
+        assertTrue(Ver.V_1_6.builder instanceof Xsd1_6)
+        assertEquals('1.6', Ver.V_1_6.number)
+        assertEquals('xsd/module-1_6.xsd', Ver.V_1_6.xsd)
+
+        assertTrue(Ver.V_1_7.builder instanceof Xsd1_7)
+        assertEquals('1.7', Ver.V_1_7.number)
+        assertEquals('xsd/module-1_7.xsd', Ver.V_1_7.xsd)
+
+        assertTrue(Ver.V_1_8.builder instanceof Xsd1_8)
+        assertEquals('1.8', Ver.V_1_8.number)
+        assertEquals('xsd/module-1_8.xsd', Ver.V_1_8.xsd)
+
+        assertTrue(Ver.V_1_9.builder instanceof Xsd1_9)
+        assertEquals('1.9', Ver.V_1_9.number)
+        assertEquals('xsd/module-1_9.xsd', Ver.V_1_9.xsd)
+    }
+}

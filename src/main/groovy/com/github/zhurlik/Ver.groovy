eb3d4ededfdf88110dc21cb5d7cb7a64120f8203
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2019-01-17 10:24:42 +0300

    codenarc violations

--- src/main/groovy/com/github/zhurlik/Ver.groovy
+++ src/main/groovy/com/github/zhurlik/Ver.groovy
@@ -1,5 +1,8 @@
 package com.github.zhurlik
 
+import static java.io.File.separator
+import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI
+
 import com.github.zhurlik.extension.JBossModule
 import com.github.zhurlik.tag.ConfigurationTag
 import com.github.zhurlik.tag.ModuleAbsentTag
@@ -18,9 +21,6 @@ import java.nio.file.Paths
 import java.util.function.Supplier
 import java.util.stream.Stream
 
-import static java.io.File.separator
-import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI
-
 /**
  * Supported versions of JBoss Modules
  *
@@ -28,10 +28,10 @@ import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI
  */
 @Slf4j
 enum Ver {
-    V_1_0('1.0', 'xsd/module-1_0.xsd'){
+    V_1_0('1.0', 'xsd/module-1_0.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return Paths.get('modules',
+            Paths.get('modules',
                     jbModule.moduleName.split('\\.').join(separator),
                     ((jbModule.slot in [null, '']) ? 'main' : jbModule.slot))
         }
@@ -40,8 +40,8 @@ enum Ver {
         String getXmlDescriptor(JBossModule jmodule) {
             Objects.requireNonNull(jmodule, 'JBossModule is null')
 
-            def writer = new StringWriter()
-            def xml = new MarkupBuilder(writer)
+            Writer writer = new StringWriter()
+            MarkupBuilder xml = new MarkupBuilder(writer)
 
             XmlDeclarationTag.write().accept(xml)
 
@@ -52,13 +52,13 @@ enum Ver {
                 ModuleTag.write(jmodule).accept(xml)
             }
 
-            return writer.toString()
+            writer.toString()
         }
     },
-    V_1_1('1.1', 'xsd/module-1_1.xsd'){
+    V_1_1('1.1', 'xsd/module-1_1.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return Paths.get('modules',
+            Paths.get('modules',
                     jbModule.moduleName.split('\\.').join(separator),
                     ((jbModule.slot in [null, '']) ? 'main' : jbModule.slot))
         }
@@ -67,8 +67,8 @@ enum Ver {
         String getXmlDescriptor(JBossModule jmodule) {
             Objects.requireNonNull(jmodule, 'JBossModule is null')
 
-            def writer = new StringWriter()
-            def xml = new MarkupBuilder(writer)
+            Writer writer = new StringWriter()
+            MarkupBuilder xml = new MarkupBuilder(writer)
 
             XmlDeclarationTag.write().accept(xml)
 
@@ -82,24 +82,24 @@ enum Ver {
                 ModuleTag.write(jmodule).accept(xml)
             }
 
-            return writer.toString()
+            writer.toString()
         }
     },
-    V_1_2('1.2', 'xsd/module-1_2.xsd'){
+    V_1_2('1.2', 'xsd/module-1_2.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return Paths.get('modules', 'system', 'layers', 'base',
+            Paths.get('modules', 'system', 'layers', 'base',
                     jbModule.moduleName.split('\\.').join(separator),
                     ((jbModule.slot in [null, '']) ? 'main' : jbModule.slot))
         }
 
         @Override
-        String getXmlDescriptor(JBossModule jmodule) {
+        String getXmlDescriptor(final JBossModule jmodule) {
             Objects.requireNonNull(jmodule, 'JBossModule is null')
             Objects.requireNonNull(jmodule.moduleName, 'Module name is null')
 
-            def writer = new StringWriter()
-            def xml = new MarkupBuilder(writer)
+            Writer writer = new StringWriter()
+            MarkupBuilder xml = new MarkupBuilder(writer)
 
             XmlDeclarationTag.write().accept(xml)
 
@@ -111,98 +111,76 @@ enum Ver {
                 ModuleTag.write(jmodule).accept(xml)
             }
 
-            return writer.toString()
+            writer.toString()
         }
     },
-    V_1_3('1.3', 'xsd/module-1_3.xsd'){
+    V_1_3('1.3', 'xsd/module-1_3.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return V_1_2.getModulePath(jbModule)
+            V_1_2.getModulePath(jbModule)
         }
 
         @Override
         String getXmlDescriptor(JBossModule jmodule) {
-            return V_1_2.getXmlDescriptor(jmodule)
+            V_1_2.getXmlDescriptor(jmodule)
         }
     },
-    V_1_5('1.5', 'xsd/module-1_5.xsd'){
+    V_1_5('1.5', 'xsd/module-1_5.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return V_1_2.getModulePath(jbModule)
+            V_1_2.getModulePath(jbModule)
         }
 
         @Override
         String getXmlDescriptor(JBossModule jmodule) {
-            return V_1_2.getXmlDescriptor(jmodule)
+            V_1_2.getXmlDescriptor(jmodule)
         }
     },
-    V_1_6('1.6', 'xsd/module-1_6.xsd'){
+    V_1_6('1.6', 'xsd/module-1_6.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return V_1_2.getModulePath(jbModule)
+            V_1_2.getModulePath(jbModule)
         }
 
         @Override
         String getXmlDescriptor(JBossModule jmodule) {
-            return V_1_2.getXmlDescriptor(jmodule)
+            V_1_2.getXmlDescriptor(jmodule)
         }
     },
-    V_1_7('1.7', 'xsd/module-1_7.xsd'){
+    V_1_7('1.7', 'xsd/module-1_7.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return Paths.get('modules', 'system', 'layers', 'base',
+            Paths.get('modules', 'system', 'layers', 'base',
                     jbModule.moduleName.split('\\.').join(separator),
                     ((jbModule.version in [null, '']) ? 'main' : jbModule.version))
         }
 
         @Override
         String getXmlDescriptor(JBossModule jmodule) {
-            return V_1_2.getXmlDescriptor(jmodule)
+            V_1_2.getXmlDescriptor(jmodule)
         }
     },
-    V_1_8('1.8', 'xsd/module-1_8.xsd'){
+    V_1_8('1.8', 'xsd/module-1_8.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return V_1_7.getModulePath(jbModule)
+            V_1_7.getModulePath(jbModule)
         }
 
         @Override
         String getXmlDescriptor(JBossModule jmodule) {
-            return V_1_2.getXmlDescriptor(jmodule)
+            V_1_2.getXmlDescriptor(jmodule)
         }
     },
-    V_1_9('1.9', 'xsd/module-1_9.xsd'){
+    V_1_9('1.9', 'xsd/module-1_9.xsd') {
         @Override
         Path getModulePath(final JBossModule jbModule) {
-            return V_1_7.getModulePath(jbModule)
+            V_1_7.getModulePath(jbModule)
         }
 
         @Override
         String getXmlDescriptor(JBossModule jmodule) {
-            return V_1_2.getXmlDescriptor(jmodule)
+            V_1_2.getXmlDescriptor(jmodule)
         }
-    };
-
-    static final FACTORY = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI)
-
-    abstract Path getModulePath(final JBossModule jBossModule)
-
-    abstract String getXmlDescriptor(final JBossModule jBossModule)
-
-    private String number
-    private String xsdPath
-
-    Ver(final String ver, final String xsdPath) {
-        this.number = ver
-        this.xsdPath = xsdPath
-    }
-
-    String getNumber() {
-        return number
-    }
-
-    String getXsdPath() {
-        return xsdPath
     }
 
     /**
@@ -216,11 +194,33 @@ enum Ver {
                 ConfigurationTag.parse(xml),
                 ModuleAliasTag.parse(xml),
                 ModuleTag.parse(xml))
-                .filter({ final Optional it -> it.isPresent() })
+                .filter { final Optional it -> it.isPresent() }
                 .findFirst()
-                .map({ final Optional it -> it.get() })
-                .map({ final Supplier<JBossModule> it -> it.get() })
-                .orElseThrow { new IllegalArgumentException("There is a problem with parsing") }
+                .map { final Optional it -> it.get() }
+                .map { final Supplier<JBossModule> it -> it.get() }
+                .orElseThrow { new IllegalArgumentException('There is a problem with parsing') }
+    }
+
+    private static final SchemaFactory FACTORY = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI)
+
+    abstract Path getModulePath(final JBossModule jBossModule)
+
+    abstract String getXmlDescriptor(final JBossModule jBossModule)
+
+    private final String number
+    private final String xsdPath
+
+    protected Ver(final String ver, final String xsdPath) {
+        this.number = ver
+        this.xsdPath = xsdPath
+    }
+
+    String getNumber() {
+        number
+    }
+
+    String getXsdPath() {
+        xsdPath
     }
 
     /**
@@ -231,9 +231,9 @@ enum Ver {
      */
     boolean isValid(final String xml) {
         try {
-            final Schema schema = FACTORY.newSchema(new StreamSource(
+            Schema schema = FACTORY.newSchema(new StreamSource(
                     getClass().classLoader.getResourceAsStream(xsdPath)))
-            final Validator validator = schema.newValidator()
+            Validator validator = schema.newValidator()
             validator.validate(new StreamSource(new StringReader(xml)))
             return true
         } catch (all) {

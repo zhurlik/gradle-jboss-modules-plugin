c588310d546ac37bd8935fca3b2a2614bbead665
simonh5 <simonh5@illinois.edu>
2023-11-08 15:29:52 -0600

    add javadoc

--- src/test/groovy/TestUtils/XMLUtil.groovy
+++ src/test/groovy/TestUtils/XMLUtil.groovy
@@ -2,12 +2,21 @@ package TestUtils
 
 import static org.junit.Assert.assertEquals
 
+/**
+ * Provides specific asserts to compare XML strings.
+ */
 class XMLUtil {
 
     static void assertXMLStrings(String expectedString, String actualString) {
         assertXMLStrings(null, expectedString, actualString)
     }
 
+    /**
+     * Parses and compares two XML strings.
+     * @param message to print when assertion fails
+     * @param expectedString expected XML string
+     * @param actualString actual XML string
+     */
     static void assertXMLStrings(String message, String expectedString, String actualString) {
         def expected = new XmlSlurper().parseText(expectedString)
         def actual = new XmlSlurper().parseText(actualString)
